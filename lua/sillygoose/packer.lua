eb595907029bd7f4ebe3eb28c706fe7b9753f039
Bryce Soghigian <bsoghigian@gmail.com>
2025-02-17 16:49:34 -0800

    feat: adding telescope + harpoon

--- lua/sillygoose/packer.lua
+++ lua/sillygoose/packer.lua
@@ -4,11 +4,40 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  -- Telescope with specific tag and its dependency
+  use {
+    'nvim-telescope/telescope.nvim',
+      tag = '0.1.8',  -- or branch = '0.1.x'
+    requires = { 'nvim-lua/plenary.nvim' }
+  }
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
   
-  -- File Explorer + Search
-  use 'nvim-telescope/telescope.nvim'
+  use 'theprimeagen/harpoon'
+  
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      'neovim/nvim-lspconfig',
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
 
-  -- Color Schemes
-  -- use "EdenEast/nightfox.nvim"
+      -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+
+      -- Snippets
+      'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets',
+    }
+  }
 end)
-
